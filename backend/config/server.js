d52acd57da451157fe4b30312507faf82f6dc821
Yikai Zhang <yikai.zhang@shopify.com>
2021-05-20 18:03:45 -0400

    Init project.

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5e6a726cd8833b784c04e7dd810c7205'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '93c7d56a-bf5d-4332-8610-afd1350ff8f4'
+};
